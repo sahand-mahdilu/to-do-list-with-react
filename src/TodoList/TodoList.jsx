551892c907d5e3c02f0f1bed1f8da0be0faab25c
sahand mahdilu <sahand.mahdilu29@gmail.com>
2024-08-06 14:34:30 +0330

    logic: functions created

--- src/TodoList/TodoList.jsx
+++ src/TodoList/TodoList.jsx
@@ -1,14 +1,94 @@
+import { Icon } from "@iconify/react";
 import Header from "../Header/Header";
+import Todo from "../todo/Todo";
+import { useState } from "react";
 
 
 export default function TodoList() {
+
+const [todos, setTodos]=useState([])
+const [todoTitle, setTodoTitle]=useState("")
+const [status, setStatus]=useState('all')
+
+const todoTitleHandler=(event)=>{
+
+    setTodoTitle(event.target .value)
+}
+
+const addTodo =(event)=>{
+    event.preventDefault()
+
+    let newTodoObject={
+id:todos.length +1,
+title: todoTitle,
+completed:false
+    }
+
+    setTodos(prevState=> {
+
+        return [...prevState, newTodoObject]
+    })
+
+setTodoTitle("")
+
+}
+
+const removeTodo= (todoId)=>{
+
+    let newTodos = todos.filter(todo => {
+
+        return todo.id !== todoId
+    })
+    setTodos(newTodos)
+
+}
+
+const editTodo=(todoId)=>{
+let newTodos= [...todos]
+
+newTodos.forEach(todo =>{
+
+    if(todo.id === todoId){
+        todo.completed= !todo.completed
+    }
+})
+
+setTodos(newTodos)
+
+}
+
+
+const statusHandler = (event) => {
+    setStatus(event.target.value)
+}
+
+
+
+
   return (
    <>
    <Header/>
    <form >
     <input type="text" />
-    <button type="submit"></button>
+    <button type="submit">
+    <Icon icon="flat-color-icons:plus" />
+    </button>
+    <select name="todos" id="">
+        <option value="all">all</option>
+        <option value="completed">completed</option>
+        <option value="uncompleted">uncompleted</option>
+    </select>
    </form>
+
+   <div className="todo-container">
+<ul>
+
+    <Todo/>
+</ul>
+
+
+
+   </div>
    
    </>
   )
--- src/todo/Todo.jsx
+++ src/todo/Todo.jsx
@@ -1,9 +1,13 @@
+import { Icon } from "@iconify/react";
 
 
 export default function Todo() {
   return (
     <div>
-        
+      <li className="todo item"></li>  
+
+      <button className="check-btn"><Icon icon="icon-park:check-one" /></button>
+      <button className="trash-btn"><Icon icon="flat-color-icons:empty-trash" /></button>
       
     </div>
   )
